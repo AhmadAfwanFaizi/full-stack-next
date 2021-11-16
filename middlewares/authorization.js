ce27cd8535c620c47ff5bf86c29c07f269e87568
AhmadAfwanFaizi <ahmadafwanfaizi@gmail.com>
2021-11-16 15:52:05 +0700

    implementasi authorization middleware

---
+++ middlewares/authorization.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+export default function authorization(req, res) {
+  return new Promise((resolve, reject) => {
+    try {
+      const { authorization } = req.headers;
+      if (!authorization) return res.status(401).end();
+
+      const authSplit = authorization.split(" ");
+      const [authType, authToken] = [authSplit[0], authSplit[1]];
+
+      if (authType !== "Bearer") return res.status(401).end();
+
+      const verify = jwt.verify(authToken, "ibukuCantik");
+
+      resolve(verify);
+    } catch (error) {
+      reject(res.status(401).end());
+    }
+  });
+}
---
+++ pages/About.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default About;
